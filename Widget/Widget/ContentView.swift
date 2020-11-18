549a8a52b4c64740f96479b1c2b5803ae6bed2ef
Derek <whc418600465@gmail.com>
2020-11-17 17:15:05 -0800

    first commit

---
+++ Widget/Widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Widget
+//
+//  Created by Derek W on 11/17/20.
+//  Copyright © 2020 Derek W. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
